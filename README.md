7296bf2aac4f19969ae3607231853931e5bc0031
Andrey Chibisov <andrey@evolveconsulting.com.hk>
2017-01-24 15:13:34 +0800

    Translation file Added. README Updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # bash_translate
+
+This uses Yandex Translate API to commit translations.
+
+You need to obtain a token from [this page](https://tech.yandex.com/translate/).
+
+Then insert it to `.ya_token` file as
+
+```
+my.yandex.translate.token
+```
+
+and you can finally run it with
+
+```bash
+./translate.py fr Привет
+```
---
+++ translate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import requests
+import sys
+
+config_file = '.ya_token'
+
+access_token = os.getenv('YANDEX_TRANSLATE_API_TOKEN')
+if not access_token or len(access_token) == 0:
+	f = open(config_file, 'r')
+	access_token = f.readline().strip()
+detect_language_url = 'https://translate.yandex.net/api/v1.5/tr.json/detect?key={}'.format(
+	access_token
+)
+translate_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key={}'.format(
+	access_token
+)
+to_lang = sys.argv[1]
+text = sys.argv[2]
+detected_response = requests.get(detect_language_url+'&text='+text)
+detected_lang = detected_response.json()['lang']
+	
+translation_response = requests.get(
+	translate_url+'&text='+text+
+	'&lang='+detected_lang+'-'+to_lang
+)
+for translated in translation_response.json()['text']:
+	print(translated)
+
